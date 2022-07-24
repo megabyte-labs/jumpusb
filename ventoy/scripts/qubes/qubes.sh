563a8b4713ce0bdbc508955140721959661575d0
Brian Zalewski <brian@megabyte.space>
2022-07-24 05:18:40 +0000

    Update ventoy/scripts/qubes/qubes.sh

--- ventoy/scripts/qubes/qubes.sh
+++ ventoy/scripts/qubes/qubes.sh
@@ -2,11 +2,12 @@
 # https://gitlab.com/megabyte-labs/jumpusb/-/raw/master/ventoy/scripts/qubes/qubes.sh
 # https://github.com/endeavouros-team/endeavouros-xfce4-theming
 sudo qubesctl --show-output state.sls update.qubes-dom0
+sudo qubes-dom0-update qubes-template-fedora-36
+sudo qubes-dom0-update qubes-template-fedora-36-xfce4
+sudo qubes-dom0-update qubes-template-debian-11
+sudo qubes-dom0-update qubes-template-debian-11-minimal
+sudo qubes-dom0-update qubes-template-debian-10-minimal
 sudo qubes-dom0-update -y kernel-latest
-sudo qubes-dom0-update -y qubes-template-fedora-36
-sudo qubes-dom0-update -y qubes-template-debian-11
-sudo qubes-dom0-update -y qubes-template-debian-11-minimal
-sudo qubes-dom0-update -y qubes-template-debian-10-minimal
 sudo qubes-dom0-update -y qubes-repo-contrib
 sudo qubes-dom0-update -y qubes-repo-templates
 sudo qubes-dom0-update -y qubes-windows-tools
@@ -16,27 +17,38 @@ sudo qubesctl --show-output --skip-dom0 --standalones state.sls update.qubes-vm
 
 # https://github.com/Jeeppler/qubes-cheatsheet/blob/master/qubes-cheatsheet.md
 
-sudo qubesctl state.sls qvm.usb-keyboard
-sudo qubesctl state.sls qvm.usb-mouse
+# sys-usb
 sudo qubesctl state.sls qvm.sys-usb
-sudo echo "sys-keyboard dom0 ask,default_target=dom0" >> /etc/qubes-rpc/policy/qubes.InputKeyboard
-sudo echo "sys-mouse dom0 ask,default_target=dom0" >> /etc/qubes-rpc/policy/qubes.InputMouse
 
+# sys-keyboard
+# qvm-clone sys-usb sys-keyboard
+sudo qubesctl state.sls qvm.usb-keyboard
+# sudo echo "sys-keyboard dom0 ask,default_target=dom0" > /etc/qubes-rpc/policy/qubes.InputKeyboard
+
+# sys-mouse
+# qvm-clone sys-usb sys-mouse
+sudo qubesctl state.sls qvm.usb-mouse
+sudo echo "sys-mouse dom0 ask,default_target=dom0" > /etc/qubes-rpc/policy/qubes.InputMouse
+
+# U2F
 sudo qubes-dom0-update -y qubes-u2f-dom0
 sudo qvm-service --enable work qubes-u2f-proxy
 sudo echo "$anyvm sys-usb allow,user=root" > /etc/qubes-rpc/policy/u2f.Authenticate
 sudo echo "$anyvm sys-usb allow,user=root" > /etc/qubes-rpc/policy/u2f.Register
 
-# GUI-VM / GPU Passthrough
-sudo qubesctl top.enable qvm.sys-gui-gpu
-sudo qubesctl top.enable qvm.sys-gui-gpu pillar=True
-sudo qubesctl --all state.highstate
-sudo qubesctl top.disable qvm.sys-gui-gpu
-sudo qubesctl state.sls qvm.sys-gui-gpu-attach-gpu
-# Reboot
-
 # Split GPG
 sudo qubes-dom0-update -y qubes-gpg-split-dom0
+
+# GUI-VM / GPU Passthrough
+sudo qubesctl top.enable qvm.sys-gui
+sudo qubesctl top.enable qvm.sys-gui pillar=True
+sudo qubesctl --all state.highstate
+sudo qubesctl top.disable qvm.sys-gui
+
+# Updates via dom0
+sudo qubesctl state.sls qvm.updates-via-whonix dom0
+
+# Reboot
 
 # Add HVM (add --property virt_mode=hvm if you want to use a kernel from within the qube)
 qvm-create opnsense --class TemplateVM --label orange
