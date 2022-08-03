adee31dceacfef16a7124383a0e644f9dae27f6d
Brian Zalewski <brian@megabyte.space>
2022-08-03 08:30:37 +0000

    Update ventoy/scripts/qubes/qubes.sh

--- ventoy/scripts/qubes/qubes.sh
+++ ventoy/scripts/qubes/qubes.sh
@@ -3,47 +3,45 @@
 # https://github.com/endeavouros-team/endeavouros-xfce4-theming
 # TODO: Temporarily disable auto-logout or figure out why USB keyboard events are blocked during install
 # TODO: Ask why usb-keyboard causes system to freeze
+# https://github.com/Jeeppler/qubes-cheatsheet/blob/master/qubes-cheatsheet.md
+
+# Update system
 sudo qubesctl --show-output state.sls update.qubes-dom0
-sudo qubes-dom0-update qubes-template-fedora-36
-sudo qubes-dom0-update qubes-template-fedora-36-xfce4
-sudo qubes-dom0-update qubes-template-debian-11
-sudo qubes-dom0-update qubes-template-debian-11-minimal
-sudo qubes-dom0-update qubes-template-debian-10-minimal
-sudo qubes-dom0-update -y kernel-latest
-sudo qubes-dom0-update -y qubes-repo-contrib
-sudo qubes-dom0-update -y qubes-repo-templates
-sudo qubes-dom0-update -y qubes-windows-tools
 sudo qubes-dom0-update --clean -y
 sudo qubesctl --show-output --skip-dom0 --templates state.sls update.qubes-vm
 sudo qubesctl --show-output --skip-dom0 --standalones state.sls update.qubes-vm
 
-# https://github.com/Jeeppler/qubes-cheatsheet/blob/master/qubes-cheatsheet.md
-
-sudo qubesctl state.sls qvm.sys-usb
-
-# sys-keyboard
+# Enable USB VM along with USB keyboard settings
 # qvm-clone sys-usb sys-keyboard
-# sudo qubesctl state.sls qvm.usb-keyboard
+sudo qubesctl state.sls qvm.usb-keyboard
 # echo "sys-keyboard dom0 ask,default_target=dom0" | sudo tee /etc/qubes-rpc/policy/qubes.InputKeyboard
 
-# sys-mouse
-# qvm-clone sys-usb sys-mouse
-echo "sys-usb dom0 ask,default_target=dom0" | sudo tee /etc/qubes-rpc/policy/qubes.InputMouse
+# Show prompt before allowing USB mice
+INPUT_MOUSE_RULE="sys-usb dom0 ask,default_target=dom0"
+INPUT_MOUSE_FILE="/etc/qubes-rpc/policy/qubes.InputMouse"
+if ! grep "$INPUT_MOUSE_RULE" "$INPUT_MOUSE_FILE"; then
+  echo "$INPUT_MOUSE_RULE" | sudo tee "$INPUT_MOUSE_FILE"
+fi
+
+sudo qubes-dom0-update qubes-template-fedora-36
+# sudo qubes-dom0-update qubes-template-debian-11 # Already installed on Qubes 4.1
+sudo qubes-dom0-update qubes-template-debian-11-minimal
 
 # U2F
-sudo qubes-dom0-update -y qubes-u2f-dom0
+sudo qubes-dom0-update qubes-u2f-dom0
+sudo qvm-service --enable personal qubes-u2f-proxy
 sudo qvm-service --enable work qubes-u2f-proxy
 echo "$anyvm sys-usb allow,user=root" | sudo tee /etc/qubes-rpc/policy/u2f.Authenticate
 echo "$anyvm sys-usb allow,user=root" | sudo tee /etc/qubes-rpc/policy/u2f.Register
 
 # Split GPG
-sudo qubes-dom0-update -y qubes-gpg-split-dom0
+sudo qubes-dom0-update qubes-gpg-split-dom0
 
 # GUI-VM / GPU Passthrough
-sudo qubesctl top.enable qvm.sys-gui
-sudo qubesctl top.enable qvm.sys-gui pillar=True
-sudo qubesctl --all state.highstate
-sudo qubesctl top.disable qvm.sys-gui
+# sudo qubesctl top.enable qvm.sys-gui
+# sudo qubesctl top.enable qvm.sys-gui pillar=True
+# sudo qubesctl --all state.highstate
+# sudo qubesctl top.disable qvm.sys-gui
 
 # Updates via dom0
 sudo qubesctl state.sls qvm.updates-via-whonix dom0
